da6649032ba64047dffed54d7973f6c2d7052e01
yaelfish <winkwinksituation@gmail.com>
2019-06-25 16:42:07 +0300

    first commit

---
+++ src/app/Entities/animal.ts
@@ -0,0 +1,12 @@
+export class Animal {
+
+    name: string;
+    age: number;
+    type: string;
+
+    constructor(name: string, age: number, type: string) {
+        this.name = name;
+        this.age = age;
+        this.type = type;
+    }
+}
